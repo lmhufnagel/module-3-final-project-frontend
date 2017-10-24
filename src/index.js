cd817eca467d14c7ab8610517fdd0c2c2f47cee4
lmhufnagel <lmhufnagel@gmail.com>
2017-10-24 11:53:53 -0400

    frontend start

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-document.onload = (function () {
+document.addEventListener('DOMContentLoaded', function() {
+  const houseDiv = document.getElementById("houses")
   getHouses()
-})
 
-getHouses() {
-  fetch('')
+function getHouses() {
+  fetch('http://localhost:3000/haunted_houses')
+  .then(res => res.json())
+  .then(json => console.log(json))
 }
+
+function renderHouses(json) {
+  json.forEach(house => {
+
+    const newHouse = new HauntedHouse(house)
+    console.log(newHouse);
+    const newHouseDiv = document.createElement('div')
+    const houseImg = document.createElement('img')
+    houseImg.setAttribute('src', `${newHouse.image}`)
+    houseImg.setAttribute('height', 500)
+    houseImg.setAttribute('width', 500)
+    newHouseDiv.appendChild(houseImg)
+    houseDiv.appendChild(newHouseDiv)
+
+  })
+
+}
+})
