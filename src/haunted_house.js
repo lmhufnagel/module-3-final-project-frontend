74e7a7996ad4c58fe47d93ca3b7cac1670d2081d
Ryan Sperzel <ryansperzel@gmail.com>
2017-10-24 11:20:42 -0400

    edit file location for index.js

--- src/haunted_house.js
+++ src/haunted_house.js
@@ -1,7 +1,10 @@
-class HauntedHouse{
+class HauntedHouse {
 
-  constructor(){
-
+  constructor(name, description, location, image){
+    this.name = name
+    this.description = description
+    this.location = location
+    this.image = image
   }
 
 }
