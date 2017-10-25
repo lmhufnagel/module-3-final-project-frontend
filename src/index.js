8223be744b3d9bbf4b5a30c6ecae64b6da4cb78b
lmhufnagel <lmhufnagel@gmail.com>
2017-10-25 14:02:45 -0400

    Reviews displaying

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ function getHouses() {
   .then(json => renderHouses(json))
 }
 
+
+
 function renderHouses(json) {
   json.forEach(house => {
 
     const newHouse = new HauntedHouse(house.id, house.name, house.description, house.location, house.image)
     console.log(newHouse);
     const newHouseDiv = document.createElement('div')
+    newHouseDiv.setAttribute('id', `div-${newHouse.id}`)
 
     const houseTitle = document.createElement('h3')
     houseTitle.innerText = newHouse.name
@@ -47,15 +50,33 @@ function renderHouses(json) {
     reviewForm.appendChild(br4)
     reviewForm.appendChild(reviewSubmit)
 
-    const displayReviews = document.createElement('div')
+    // const displayReviews = document.createElement('div')
     // const reviewBody =
 
+    const showReviewsButton = document.createElement("button")
+    showReviewsButton.innerText = "Reviews"
+    showReviewsButton.dataset.id = newHouse.id
+
+    showReviewsButton.addEventListener('click', e => {
+      fetch(`http://localhost:3000/api/haunted_houses/${reviewForm.dataset.id}`)
+      .then(res => res.json())
+      .then(json => {
+        json.reviews.forEach(review => {
+          const reviewtag = document.createElement('p')
+          reviewtag.innerHTML = `${review.name} <br> ${review.body}`
+          const divPointer = document.getElementById(`div-${reviewForm.dataset.id}`)
+          divPointer.appendChild(reviewtag)
+        })
+      })
+    })
 
     newHouseDiv.appendChild(houseTitle)
     newHouseDiv.appendChild(houseImg)
     newHouseDiv.appendChild(houseDesc)
     newHouseDiv.appendChild(reviewForm)
     houseDiv.appendChild(newHouseDiv)
+    houseDiv.appendChild(showReviewsButton)
+
 
     reviewForm.addEventListener('submit', e => {
       e.preventDefault()
@@ -67,13 +88,43 @@ function renderHouses(json) {
           "Content-Type": "application/json"
         }
     })
-    .then(res => {
-      console.log(res)
-    })
+    e.target[0].value = ""
+    e.target[1].value = ""
     })
 
+// <button onclick="toggleClock()" id="clockButton">Show clock</button>
   })
-
+//   showReviewsButton.addEventListener('click', e => displayReviews())
+//
+// function displayReviews() {
+//
+//   fetch(`http://localhost:3000/api/${reviewForm.dataset.id}`)
+//   .then(res => res.json())
+//   .then(json => )
+//
+//     // get the clock
+//     const allReviews = ;
+//
+//     // get the current value of the clock's display property
+//     var displaySetting = myClock.style.display;
+//
+//     // also get the clock button, so we can change what it says
+//     var clockButton = document.getElementById('clockButton');
+//
+//     // now toggle the clock and the button text, depending on current state
+//     if (displaySetting == 'block') {
+//       // clock is visible. hide it
+//       myClock.style.display = 'none';
+//       // change button text
+//       clockButton.innerHTML = 'Show clock';
+//     }
+//     else {
+//       // clock is hidden. show it
+//       myClock.style.display = 'block';
+//       // change button text
+//       clockButton.innerHTML = 'Hide clock';
+//     }
+//   }
 }
 
 })
